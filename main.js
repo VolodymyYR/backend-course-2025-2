09b89e2ff15f5875f2edfd213480aad9fc7e1cdc
VolodymyrYR <anifeelings02@gmail.com>
2025-09-24 11:06:20 +0300

    Результат

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const { URL } = require('url');
 
 // Константи для Варіанту 4
 const currency = 'EUR';
-const exact_date = '20240910';
+const exact_date = '10.09.2024';
 
 // Створення URL з одразу доданими параметрами
-const apiUrl = new URL('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange');
+const apiUrl = new URL('https://bank.gov.ua/NBU_ovdp');
 apiUrl.searchParams.set('valcode', currency);
 apiUrl.searchParams.set('date', exact_date);
 apiUrl.searchParams.set('json', '');
